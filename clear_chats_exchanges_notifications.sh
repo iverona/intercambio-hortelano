163ddb9c400661dc3b0a7c71531a8fd40decf05a
Ignacio Verona <iverona@estudio-verona.com>
2026-01-25 23:35:03 +0100

    feat: Create script to recursively delete specified Firestore collections.

---
+++ clear_chats_exchanges_notifications.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script deletes all specified Firestore collections.
+# WARNING: This is a destructive action and cannot be undone.
+
+echo "Deleting Firestore collections..."
+
+firebase firestore:delete chats --recursive -f
+firebase firestore:delete notifications --recursive -f
+firebase firestore:delete exchanges --recursive -f
+
+echo "All specified Firestore collections have been deleted."
